1d208877bc5abc8dfa1550262c0c1feaf96f8b23
SemihBG <buyukgungorsemih@gmail.com>
2021-04-23 23:51:42 +0300

    server environment setup, configuration and docker compose

---
+++ bundle-sharing-server/src/main/java/com/smh/bs/server/ApiController.java
@@ -0,0 +1,11 @@
+package com.smh.bs.server;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+
+
+}
