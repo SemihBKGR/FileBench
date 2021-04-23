c92922681bc240df8c94628ba5ee46f43e3873e1
SemihBG <buyukgungorsemih@gmail.com>
2021-04-23 23:15:28 +0300

    server project init

---
+++ bundle-sharing-server/src/main/java/com/smh/bs/server/BundleSharingServerApplication.java
@@ -0,0 +1,13 @@
+package com.smh.bs.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BundleSharingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BundleSharingServerApplication.class, args);
+    }
+
+}
---
+++ bundle-sharing-server/src/test/java/com/smh/bs/server/BundleSharingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smh.bs.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BundleSharingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
