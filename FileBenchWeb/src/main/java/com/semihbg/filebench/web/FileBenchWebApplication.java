d58db77f0f3ccb769f1e89c3498d081dc6eb1c4c
SemihBG <buyukgungorsemih@gmail.com>
2021-06-01 23:53:28 +0300

    Web project initialization

---
+++ FileBenchWeb/src/main/java/com/semihbg/filebench/web/FileBenchWebApplication.java
@@ -0,0 +1,13 @@
+package com.semihbg.filebench.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileBenchWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileBenchWebApplication.class, args);
+    }
+
+}
---
+++ FileBenchWeb/src/test/java/com/semihbg/filebench/web/FileBenchWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semihbg.filebench.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileBenchWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
