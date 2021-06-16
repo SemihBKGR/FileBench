95607635cf4f0291e4a20c174325ef24576e67f6
SemihBG <buyukgungorsemih@gmail.com>
2021-06-16 23:59:27 +0300

    Android main and menu activity components

--- FileBenchAndroid/app/src/main/java/com/semihbg/filebench/android/MenuActivity.java
+++ FileBenchAndroid/app/src/main/java/com/semihbg/filebench/android/MenuActivity.java
@@ -1,6 +1,7 @@
 package com.semihbg.filebench.android;
 
 import android.content.Intent;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +9,8 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class MenuActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
