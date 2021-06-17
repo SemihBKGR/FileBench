9175c4235a02f68de4738e5a36c92642c0c98337
SemihBG <buyukgungorsemih@gmail.com>
2021-06-17 23:53:00 +0300

    Android bench and create activity

---
+++ FileBenchAndroid/app/src/main/java/com/semihbg/filebench/android/CreateActivity.java
@@ -0,0 +1,14 @@
+package com.semihbg.filebench.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create);
+    }
+
+}
