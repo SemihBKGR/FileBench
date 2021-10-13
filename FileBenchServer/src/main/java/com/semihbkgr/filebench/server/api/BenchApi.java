6a58c05da9a0c709b26ae58eaa6495df0b8f8d78
SemihBG <buyukgungorsemih@gmail.com>
2021-10-14 01:06:49 +0300

    Server BenchApi save file with its extension, validation config properties review

--- FileBenchServer/src/main/java/com/semihbkgr/filebench/server/api/BenchApi.java
+++ FileBenchServer/src/main/java/com/semihbkgr/filebench/server/api/BenchApi.java
@@ -68,7 +68,8 @@ public class BenchApi {
     @ResponseStatus(HttpStatus.CREATED)
     public Mono<File> createFile(@PathVariable("bench_id") String benchId,
                                  @RequestPart("content") Mono<FilePart> filePartMono,
-                                 @RequestParam("token") String token) {
+                                 @RequestParam("token") String token,
+                                 @RequestHeader("Content-Length") long contentLength) {
         return benchService.findById(benchId)
                 .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Bench not found")))
                 .flatMap(bench -> {
@@ -76,19 +77,17 @@ public class BenchApi {
                         return Mono.error(new ResponseStatusException(HttpStatus.FORBIDDEN, "Incorrect token"));
                     if (bench.getFiles() == null)
                         bench.setFiles(new ArrayList<>());
-
-                    return filePartMono.map(filePart -> {
-                        return fileOf(filePart.filename(),filePart.)
-                    }).flatMap(file->fileValidator.validate(file))
+                    var file = fileOf(contentLength);
+                    bench.getFiles().add(file);
+                    return filePartMono
+                            .doOnNext(filePart -> file.setName(filePart.filename()))
+                            .then(fileValidator.validate(file))
                             .flatMap(validationResult -> {
                                 if (validationResult.isInvalid())
                                     return Mono.error(new ValidationException(validationResult));
-                                else {
-                                    bench.getFiles().add(file);
-                                    return storageService.saveFile(benchId, file.getId(), filePartMono)
-                                            .then(benchService.update(benchId, bench))
-                                            .thenReturn(file);
-                                }
+                                return storageService.saveFile(benchId, file.getId(), filePartMono)
+                                        .then(benchService.update(benchId, bench))
+                                        .thenReturn(file);
                             });
                 });
     }
@@ -198,7 +197,9 @@ public class BenchApi {
                         return Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "File not found"));
                     var file = fileOptional.get();
                     file.setSize(contentLength);
-                    return storageService.updateFile(benchId, fileId, filePartMono)
+                    return filePartMono
+                            .doOnNext(filePart -> file.setName(replaceExtension(file.getName(), extractExtension(filePart.filename()))))
+                            .then(storageService.updateFile(benchId, fileId, filePartMono))
                             .then(benchService.update(benchId, bench))
                             .then(Mono.just(file));
                 });
@@ -218,7 +219,7 @@ public class BenchApi {
                 .build();
     }
 
-    private File fileOf(String name,long size) {
+    private File fileOf(long size) {
         return File.builder()
                 .id(idGenerator.generate())
                 .name(null)
@@ -227,6 +228,21 @@ public class BenchApi {
                 .size(size)
                 .downloadCount(0L)
                 .build();
+    }
+
+    private String extractExtension(@NonNull String filename) {
+        if (filename.contains("."))
+            return filename.substring(filename.lastIndexOf('.')+1);
+        return null;
+    }
+
+    private String replaceExtension(@NonNull String filename, String extension) {
+        if (extension == null)
+            return filename;
+        if (filename.contains("."))
+            return filename.substring(0, filename.lastIndexOf('.')).concat(".").concat(extension);
+        else
+            return filename.concat(".").concat(extension);
     }
 
 }
